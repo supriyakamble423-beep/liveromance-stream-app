4f800105a87473bc6d1b99291b49c7429c4b08a2
Supriya Kamble <supriyakamble423@gmail.com>
2026-02-20 12:46:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3169873612-ad502",
+  "appId": "1:1074761045103:web:6a623b6a0775c8719de99a",
+  "apiKey": "AIzaSyCTw7JXMLn6HEcTYvHUNNHr7U65OXDFazQ",
+  "authDomain": "studio-3169873612-ad502.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1074761045103"
+};
